eae72a69aa17812d76b3194ba58667b1bc874321
Asamuel78 <Therapybyanthony@gmail.com>
2015-08-26 16:29:35 -0400

    First commit

---
+++ guessing_game.rb
@@ -0,0 +1,14 @@
+numb = rand(100)
+puts numb
+puts "Guess a number between 0 and 100"
+guess = gets
+
+# while (true)
+#   if numb > rand(100)
+#   puts "Your guess is too high, guess again"
+#   else numb < rand(100)
+#       puts "Your guess is too low, guess again"
+#   elsif numb = rand(100)
+#       puts "Your guess is correct"
+#   break
+# end
