d15bb3599021825469105decb11bfb23561f9c55
Asamuel78 <Therapybyanthony@gmail.com>
2015-08-28 09:56:47 -0400

    commit#2

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,17 +1,26 @@
-numb = rand(100)
-puts "Guess a number between 0 and 100"
-guess = gets.chomp.to_i
-
-while (true)
-  if guess > numb
-    puts "Your guess is too high, guess again"
-  elsif guess == numb
-    puts "Your guess is correct"
-    break
-  else guess < numb
-    puts "Your guess is too low, guess again"
+def play(numb)
+  5.times do
+    guess = gets.chomp.to_i
+    if guess > numb
+      puts "Your guess was too high"
+    elsif guess == numb
+      puts "Your guess was correct"
+      break
+    else guess < numb
+      puts "Your guess was too low"
+    end
   end
-
-  puts "Try again"
-  guess = gets.chomp.to_i
+  puts "THAT WAS THE LAST TRYYYY"
 end
+
+def number_game
+  puts "Guess a number between 0 and 100"
+end
+
+numb = rand(100)
+puts numb
+number_game
+
+play(numb)
+
+puts "Thanks for coming, try again soon."
